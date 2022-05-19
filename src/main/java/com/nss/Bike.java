e5c07124f6ca0018759cae45e8a4b55b67acef1c
Your Name <you@example.com>
2022-05-19 16:06:24 +0530

    Car and Bike classes are deleted

--- src/main/java/com/nss/Bike.java
+++
@@ -1,9 +0,0 @@
-package com.nss;
-
-public class Bike {
-
-	public void startBike() {
-		// Logic as per the HIS-134 goes here
-	}
-	
-}
--- src/main/java/com/nss/Car.java
+++
@@ -1,9 +0,0 @@
-package com.nss;
-
-public class Car {
-
-	public void start() {
-		// Logic as per HIS-122 task needs to be done
-	}
-	
-}
