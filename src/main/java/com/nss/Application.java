cdff28743277206f516ccc8e962b565130ea44a5
Your Name <you@example.com>
2022-05-19 18:16:31 +0530

    Added Map variablt to load Cache

--- src/main/java/com/nss/Application.java
+++ src/main/java/com/nss/Application.java
@@ -6,10 +6,15 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
+	private static final Map<String, Object> cache = new HashMap<String, Object>();
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class, args);
 		
+	}
+
+	public void loadCacheData(){
+		// Logic 
 	}
 
 }
